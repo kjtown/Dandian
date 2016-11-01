5d7c9f4349a3a05e99e144791ead376ccc87beb3
懒人懒人 <18237358244@163.com>
2016-11-01 20:46:55 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/asus/dandian/MainActivity.java
@@ -0,0 +1,38 @@
+package com.bawei.asus.dandian;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://blog.csdn.net/xiaanming"));
+                startActivity(intent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+}
